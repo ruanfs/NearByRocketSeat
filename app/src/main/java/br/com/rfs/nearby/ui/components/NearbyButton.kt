1eea6e118823d16b079fb4d3c38278a254248926
Ruan Ferreira <8647151+ruan4f@users.noreply.github.com>
2025-01-02 22:59:09 -0300

    Initial commit

---
+++ app/src/main/java/br/com/rfs/nearby/ui/components/NearbyButton.kt
@@ -0,0 +1,25 @@
+package br.com.rfs.nearby.ui.components
+
+import androidx.annotation.DrawableRes
+import androidx.compose.material3.Button
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun NearbyButton(
+    modifier: Modifier = Modifier,
+    text: String? = null,
+    @DrawableRes iconRes: Int?=null,
+    onClick: () -> Unit
+) {
+    Button(modifier = modifier, onClick = onClick) {
+
+    }
+}
+
+@Preview
+@Composable
+private fun NearbyButtonPreview() {
+    NearbyButton(modifier = Modifier, text = "Confirmar", iconRes = null) {}
+}
